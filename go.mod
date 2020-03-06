2bafb992f6c0698d164629d10fb18c2395bd08a4
Kamil Matysiewicz <kamil@graphqleditor.com>
2020-03-06 16:16:21 +0100

    refactor: move azure helpers to main stucco package

--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/graphql-editor/stucco-router-azure-worker
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.3.4 // indirect
 	github.com/graphql-editor/azure-functions-golang-worker v0.0.0-20200227151538-8f683507a71c
-	github.com/graphql-editor/stucco v0.3.4-0.20200304124034-675bdc198274
+	github.com/graphql-editor/stucco v0.3.4-0.20200306151245-07ccfddf19ac
+	github.com/graphql-go/graphql v0.7.9 // indirect
 	github.com/graphql-go/handler v0.2.3
-	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.4.0
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
+	google.golang.org/genproto v0.0.0-20200305110556-506484158171 // indirect
+	google.golang.org/grpc v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- graphql/function.go
+++ graphql/function.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/graphql-editor/azure-functions-golang-worker/api"
-	azuredriver "github.com/graphql-editor/stucco-router-azure-worker/driver"
-	azurehandler "github.com/graphql-editor/stucco-router-azure-worker/handler"
 	"github.com/graphql-editor/stucco/pkg/driver"
+	"github.com/graphql-editor/stucco/pkg/handlers"
+	azuredriver "github.com/graphql-editor/stucco/pkg/providers/azure/driver"
+	azurehandler "github.com/graphql-editor/stucco/pkg/providers/azure/handler"
 	"github.com/graphql-editor/stucco/pkg/router"
 	"github.com/graphql-editor/stucco/pkg/utils"
 	gqlhandler "github.com/graphql-go/handler"
@@ -17,39 +17,7 @@ import (
 
 var (
 	handler azurehandler.Handler
-	config  = "./stucco"
 )
-
-func withProtocolInContext(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(
-			rw,
-			r.WithContext(
-				context.WithValue(
-					r.Context(),
-					router.ProtocolKey, map[string]interface{}{
-						"headers": r.Header,
-					},
-				),
-			),
-		)
-	})
-}
-
-func recoveryHandler(next http.Handler, logger api.Logger) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		defer func() {
-			err := recover()
-			if err != nil {
-				logger.Errorf("%v\n", err)
-				rw.Header().Set("Content-Type", "text/plain")
-				rw.WriteHeader(http.StatusInternalServerError)
-				rw.Write([]byte("There was an internal server error"))
-			}
-		}()
-		next.ServeHTTP(rw, r)
-	})
-}
 
 // HTTPTrigger is an example httpTrigger
 type HTTPTrigger struct {
@@ -75,13 +43,7 @@ func init() {
 		Runtime:  "function",
 	})
 	var cfg router.Config
-	if schema := os.Getenv("STUCCO_SCHEMA"); schema != "" {
-		cfg.Schema = schema
-	}
-	if envConfig := os.Getenv("STUCCO_CONFIG"); envConfig != "" {
-		config = envConfig
-	}
-	if err := utils.LoadConfigFile(config, &cfg); err != nil {
+	if err := utils.LoadConfigFile("", &cfg); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
@@ -90,7 +52,7 @@ func init() {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 	}
 	handler = azurehandler.Handler{
-		Handler: withProtocolInContext(gqlhandler.New(&gqlhandler.Config{
+		Handler: handlers.WithProtocolInContext(gqlhandler.New(&gqlhandler.Config{
 			Schema:   &router.Schema,
 			Pretty:   true,
 			GraphiQL: true,
